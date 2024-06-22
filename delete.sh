c2d2fc791c39a3abab59c7f5249df11a6ecbaf99
Tien Anh <99895433+tienanh109@users.noreply.github.com>
2024-06-22 09:57:18 +0700

    Delete delete.sh

--- delete.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "The name of the command you want to delete from the Termux command you created: > Name: " cmd_name
-
-sed -i "/alias $cmd_name=/d" ~/.bashrc
-source ~/.bashrc
-
-echo "Command '$cmd_name' deleted successfully!"
